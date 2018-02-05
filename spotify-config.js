ccc94cbdd98c149286c410352031561d59077dab
Sitian Liu <goldensunliu@gmail.com>
2018-02-04 22:58:45 -0500

    update dep

--- spotify-config.js
+++ spotify-config.js
@@ -1,2 +1,4 @@
-export const CLIENT_ID = null // Set this to your Spotify app's client ID;
-export const APOLLO_ENGINE_API_KEY = null; // optionally set this if you want apollo engine in to mix
+export const CLIENT_ID = 'c538eead5dee4390a12f093bec9462fa';
+export const APOLLO_ENGINE_API_KEY = 'service:goldensunliu-3759:GnYvhMHy1u160NHQQhq-sA';
+export const SENTRY_DSN = 'https://81600deaf34d4c419841bc8a773d67f6@sentry.io/228299';
+export const SCOPES = 'user-read-recently-played+user-library-read+user-library-modify+playlist-read-private+user-top-read';
