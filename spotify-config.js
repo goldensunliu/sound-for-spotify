af91aeabe1f86e315b3d6f31e9dbbea1dc58e0eb
Sitian Liu <goldensunliu@gmail.com>
2018-02-06 13:42:20 -0500

    fix scopes

--- spotify-config.js
+++ spotify-config.js
@@ -1,4 +1,7 @@
 export const CLIENT_ID = 'c538eead5dee4390a12f093bec9462fa';
 export const APOLLO_ENGINE_API_KEY = 'service:goldensunliu-3759:GnYvhMHy1u160NHQQhq-sA';
 export const SENTRY_DSN = 'https://81600deaf34d4c419841bc8a773d67f6@sentry.io/228299';
-export const SCOPES = 'user-read-recently-played+user-library-read+user-library-modify+playlist-read-private+user-top-read';
+export const SCOPES = [
+    'user-read-recently-played', 'user-library-read', 'user-library-modify', 'playlist-read-private', 'user-top-read',
+    'playlist-modify-public','playlist-modify-private'
+]
